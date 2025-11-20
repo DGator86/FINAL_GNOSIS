6b50ec0019508623799374d44d8bc41c91c6d0bf
DGator86 <djvogeli@gmail.com>
2025-11-20 14:23:22 -0500

    Add hyperparameter optimization utilities

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,9 @@
 from .base import BaseGnosisModel
+from .hyperparameter_optimizer import BayesianOptimizer, HyperparameterOptimizer, HyperparameterSpace
+
+__all__ = [
+    "BaseGnosisModel",
+    "BayesianOptimizer",
+    "HyperparameterOptimizer",
+    "HyperparameterSpace",
+]
